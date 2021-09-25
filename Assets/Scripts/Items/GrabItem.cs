b946069dacfeaeed639535ab1fc2d7e93392bf8e
crankyMagician <74011113+studyingsam@users.noreply.github.com>
2021-09-25 16:59:27 -0400

    updated player detect script
    made logic more concise and put into a player master class, made the sample scene "Temp Scene", added items

---
+++ Assets/Scripts/Items/GrabItem.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GrabItem : MonoBehaviour
+{
+
+    [Tooltip("This is the logic of the player")]
+    private Player player;
+
+
+
+    [SerializeField]
+    private ItemDataContainer itemDataContainer = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        StartCoroutine(FindComponents());
+    }
+
+
+    IEnumerator FindComponents()
+    {
+
+        player = FindObjectOfType<Player>();
+
+        yield return new WaitForEndOfFrame();
+
+    }
+
+    //when the player enters the trigger of the item and item on the player is null
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && player.playerAction && player.item == null)
+        {
+            player.item = this;
+
+        }
+    }
+}
+

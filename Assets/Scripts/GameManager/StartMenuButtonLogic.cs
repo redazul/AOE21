6a6df818d3542028584442e723696eb23963fe9e
crankyMagician <74011113+studyingsam@users.noreply.github.com>
2021-09-26 13:54:37 -0400

    final build

--- Assets/Scripts/GameManager/StartMenuButtonLogic.cs
+++ Assets/Scripts/GameManager/StartMenuButtonLogic.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class StartMenuButtonLogic : MonoBehaviour
 {
+    public Sprite[] backgrounds;
+    [SerializeField]
+    Image image;
+    [SerializeField]
+    public static int currentSprite = 0;
+    [SerializeField]
+
+    private GameObject startButton;
+    [SerializeField]
+
+    private GameObject NextButton;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        StartCoroutine(ChangePhoto());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ChangePhoto()
     {
-        
+        yield return new WaitForSeconds(10);
+        currentSprite++;
+        image.sprite = backgrounds[currentSprite];
+       StartCoroutine(SecondImage());
+
     }
+
+    IEnumerator SecondImage()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(1);
+    }
+    public void ChangeBackGround()
+    {
+        if (currentSprite == 0)
+        {
+            currentSprite++;
+            image.sprite = backgrounds[currentSprite];
+           
+        }
+        else if(currentSprite == 1)
+        {
+            currentSprite++;
+            image.sprite = backgrounds[currentSprite];
+        }
+        else if (currentSprite == 2)
+        {
+            currentSprite++;
+            image.sprite = backgrounds[currentSprite];
+            startButton.SetActive(true);
+            NextButton.SetActive(false);
+
+        }
+    }
+
 }
